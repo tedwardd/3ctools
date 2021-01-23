c069f30ceadfbe1a88e0302cf399e897560f4b23
Ted Wood <ted@mailchimp.com>
2021-01-22 23:08:11 -0500

    Break up the bits

--- bot_pl.py
+++ bot_pl.py
@@ -18,23 +18,60 @@ Example: When viewing my bot, my URL is "https://3commas.io/bots/1234567", my bo
 """
 
 
-def get_prices(deal, fee: float):
-    try:
-        sold = float(deal.get("sold_volume"))
-    except TypeError:
-        sold = float(0)
-    try:
-        bought = float(deal.get("bought_volume"))
-    except TypeError:
-        bought = float(0)
+class Config:
+    def __init__(self, config_file):
+        config = configparser.ConfigParser()
+        config.read(config_file)
 
-    sell_fee = sold * fee
-    buy_fee = bought * fee
-    fees = sell_fee + buy_fee
+        self.api_key = config["GLOBAL"]["api_key"]
+        self.api_secret = config["GLOBAL"]["api_secret"]
+        self.exchange_fee = config["GLOBAL"]["exchange_fee"]
 
-    real_pl = (sold - (sold * fee)) - (bought - (bought * fee))
 
-    return real_pl, fees
+class Client:
+    def __init__(self, config):
+        self.p3cw = Py3CW(
+            key=config.api_key,
+            secret=config.api_secret,
+            request_options={
+                "request_timeout": 10,
+                "nr_of_retries": 1,
+                "retry_status_codes": [502],
+            },
+        )
+
+    def deals(self, bot_id):
+        if bot_id is None:
+            error, deals = self.p3cw.request(
+                entity="deals",
+                action="",
+            )
+        else:
+            error, deals = self.p3cw.request(
+                entity="deals",
+                action="show",
+                action_id=bot_id,
+            )
+        return error, deals
+
+    def get_prices(self, deal, fee: float):
+        # TODO: Use API provided fee information once it's available
+        try:
+            sold = float(deal.get("sold_volume"))
+        except TypeError:
+            sold = float(0)
+        try:
+            bought = float(deal.get("bought_volume"))
+        except TypeError:
+            bought = float(0)
+
+        sell_fee = sold * fee
+        buy_fee = bought * fee
+        fees = sell_fee + buy_fee
+
+        real_pl = (sold - (sold * fee)) - (bought - (bought * fee))
+
+        return real_pl, fees
 
 
 @click.command()
@@ -48,62 +85,43 @@ def get_prices(deal, fee: float):
 @click.option(
     "-c",
     "--config",
+    "config_file",
     required=False,
     default="config.ini",
     help="Alternate config file. Default: config.ini",
 )
-def main(bot, config):
+def main(bot, config_file):
     """
     Calculate total P/L and Fees from DCA bot trades.
     Will calculate all deals unless bot ID specified with optional argument
     """
-    cfg = configparser.ConfigParser()
-    cfg.read(config)
-    p3cw = Py3CW(
-        key=cfg["GLOBAL"]["api_key"],
-        secret=cfg["GLOBAL"]["api_secret"],
-        request_options={
-            "request_timeout": 10,
-            "nr_of_retries": 1,
-            "retry_status_codes": [502],
-        },
-    )
+    config = Config(config_file)
+    client = Client(config)
 
-    if bot is not None:
-        error, deals = p3cw.request(
-            entity="deals",
-            action="show",
-            action_id=bot,
-        )
-    if bot is None:
-        error, deals = p3cw.request(
-            entity="deals",
-            action="",
-        )
-
+    error, deals = client.deals(bot)
     if error.get("error"):
         click.secho(error.get("msg"), fg="red", bold=True)
         sys.exit(1)
 
     total_pl = float(0)
     total_fees = float(0)
-    fee = float(cfg["GLOBAL"]["exchange_fee"]) / 100
+    fee = float(config.exchange_fee) / 100
 
     for deal in deals:
         if not deal.get("finished?"):
             continue
-        pl, fees = get_prices(deal, fee)
+        pl, fees = client.get_prices(deal, fee)
         if pl is None and fee is None:
             continue
         if deal.get("status") != "failed":
 
-            click.secho("-----", bold=True)
             click.echo(f"Deal ID: {deal.get('id')}")
+            click.secho("-----", bold=True)
             click.echo(f"P/L: ${round(pl, 2)}")
             click.echo(f"Fee: ${round(fees, 2)}")
+            click.echo()
             total_pl += pl
             total_fees += fees
-    click.echo()
     click.secho("Totals", bold=True)
     click.secho("-----", bold=True)
     click.echo(f"P/L: ${round(total_pl, 2)}")
